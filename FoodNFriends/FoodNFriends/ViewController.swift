e24b72466edcb5fe682b3d2487494dda5f2c23a3
jae <s10205286@connect.np.edu.sg>
2022-01-13 21:48:29 +0800

    Initial Commit

---
+++ FoodNFriends/FoodNFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodNFriends
+//
+//  Created by Jae on 13/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
