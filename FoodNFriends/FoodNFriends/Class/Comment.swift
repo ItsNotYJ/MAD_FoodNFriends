9546565a7fd07c1d5c086b95c529e81562aca46d
Dave <dave21sg@gmail.com>
2022-01-21 01:41:22 +0800

    added comment class

---
+++ FoodNFriends/FoodNFriends/Class/Comment.swift
@@ -0,0 +1,21 @@
+//
+//  Comment.swift
+//  FoodNFriends
+//
+//  Created by MAD2 on 20/1/22.
+//
+
+import Foundation
+
+class Comment {
+    var Username:String
+    var Comment:String
+
+    init(username:String, comment:String)
+    {
+        Username = username
+        Comment = comment
+
+    }
+
+}
