9689bbc844e3e447e6a5ce5f126c20b28fa1b4a4
Dave <dave21sg@gmail.com>
2022-01-21 01:43:02 +0800

    added RoomTableViewController code and removed RoomViewController

---
+++ FoodNFriends/FoodNFriends/ViewController/RoomTableViewController.swift
@@ -0,0 +1,64 @@
+//
+//  RoomTableViewController.swift
+//  FoodNFriends
+//
+//  Created by MAD2 on 20/1/22.
+//
+
+import Foundation
+
+import Foundation
+import UIKit
+
+class RoomTableViewController : UITableViewController {
+    
+    var roomList:[Room] = []
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        roomList = appDelegate.roomList
+        self.tableView.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        roomList = appDelegate.roomList
+        self.tableView.reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return roomList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "RoomCell", for: indexPath)
+        let room = roomList[indexPath.row]
+        cell.textLabel!.text = "\(room.Name)"
+        cell.detailTextLabel!.text = "\(room.Description)"
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "RoomContent", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "RoomContentView") as UIViewController
+        vc.modalPresentationStyle = .fullScreen
+        
+        let room = roomList[indexPath.row]
+        appDelegate.locationList = room.LocationList
+        
+        self.present(vc, animated: true, completion: nil)
+
+            
+            
+            
+            
+     
+        }
+    
+    
+}
