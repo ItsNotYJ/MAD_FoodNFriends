31d9f17cba721da0b295b02b518c0aa94fa6dc36
elginloh <elginloh@gmail.com>
2022-01-28 02:06:29 +0800

    modify room class

--- FoodNFriends/FoodNFriends/Class/Room.swift
+++ FoodNFriends/FoodNFriends/Class/Room.swift
@@ -10,14 +10,16 @@ class Room {
     var Name:String
     var Description:String
     var OwnerID:String
+    var RoomCode:String
     var LocationList:[Location] = []
     var MemberList:[String] = []
 
-    init(name:String, description:String, ownerID:String, locationList:[Location], memberList:[String])
+    init(name:String, description:String, ownerID:String, roomCode:String, locationList:[Location], memberList:[String])
     {
         Name = name
         Description = description
         OwnerID = ownerID
+        RoomCode = roomCode
         LocationList = locationList
         MemberList = memberList
     }
