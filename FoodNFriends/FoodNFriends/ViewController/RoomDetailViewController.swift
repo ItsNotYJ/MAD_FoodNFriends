420d5cef56d0e1b4230832cf8ba7dd8cac8fd8f5
Dave <dave21sg@gmail.com>
2022-01-29 02:53:17 +0800

    added function to leave room and update various firebase tables

--- FoodNFriends/FoodNFriends/ViewController/RoomDetailViewController.swift
+++ FoodNFriends/FoodNFriends/ViewController/RoomDetailViewController.swift
@@ -7,11 +7,13 @@
 
 import Foundation
 import UIKit
+import FirebaseDatabase
 
 class RoomDetailController : UIViewController {
     
     
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    private let database = Database.database().reference()
     
     var room:Room?
     var memberList:[String] = []
@@ -19,14 +21,89 @@ class RoomDetailController : UIViewController {
     @IBOutlet weak var memberTable: UITableView!
     @IBOutlet weak var nameTxt: UILabel!
     @IBOutlet weak var descTxt: UILabel!
-    @IBOutlet weak var leaveBtn: UIButton!
 
+    @IBOutlet weak var leaveBtn: UIButton!
+    
+    
     @IBAction func leaveBtn(_ sender: Any) {
         
         let alert = UIAlertController(title: "Leave Room?", message: "Do you want to leave the room?", preferredStyle: .alert)
 
+        
+        let email = AppDelegate.emailRef
+        let emailnew = email.replacingOccurrences(of: "@", with: "-")
+        let emailnewer = emailnew.replacingOccurrences(of: ".", with: "_")
+        
         alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: {_ in
             print("yes")
+            if emailnewer == self.room?.OwnerID
+            {
+                let errAlert = UIAlertController(title: "Cannot leave", message: "Don't leave! You squad needs a leader!", preferredStyle: .alert)
+                errAlert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+                self.present(errAlert, animated: true)
+            }
+            else
+            {
+                
+                 
+                
+                self.appDelegate.roomList.removeAll{room in
+                    return room.RoomCode == self.appDelegate.room?.RoomCode
+                }
+                
+                self.appDelegate.room?.MemberList.removeAll{value in
+                   return value == emailnewer}
+                let newMemberList:[String] = self.appDelegate.room!.MemberList
+                
+                
+                self.database.child("Rooms").child(self.appDelegate.room!.RoomCode).child("Members").setValue(newMemberList)
+                
+                self.database.child("Users").child(emailnewer).observeSingleEvent(of: .value, with: {snapshot in
+                    let userData = snapshot.value as? [String:Any]
+                    
+                    var roomIDList:[String] = []
+                    
+                    
+                    if userData!["RoomList"] == nil {
+                        print("error")
+                    }
+                    else
+                    {
+                        let roomlist = userData!["RoomList"] as! NSArray
+                        
+                        
+                        for roomid in roomlist
+                        {
+                            
+                            roomIDList.append((roomid as? String)!)
+                        }
+                        
+                    }
+                    
+                    roomIDList.removeAll{x in
+                        return x == self.appDelegate.room?.RoomCode
+                    }
+                    
+                    
+                    self.database.child("Users").child(emailnewer).child("RoomList").setValue(roomIDList)
+                
+                
+                
+                })
+                
+                self.appDelegate.hawkerCentreList = []
+                self.appDelegate.roomList = []
+                
+                AppDelegate.emailRef = ""
+                
+                let successAlert = UIAlertController(title: "Success", message: "You have left the room", preferredStyle: .alert)
+                successAlert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+                self.dismiss(animated: true, completion: nil)
+                self.present(successAlert, animated: true)
+                
+                
+            }
+            
         }))
         alert.addAction(UIAlertAction(title: "No", style: .default, handler: nil))
         self.present(alert, animated: true)
