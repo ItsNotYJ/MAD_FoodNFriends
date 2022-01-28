79b3842d979ddfd39bf072a84f9716fe1fda4f35
jae <s10205286@connect.np.edu.sg>
2022-01-28 12:44:16 +0800

    Add Location Google Maps View to detailed view

--- FoodNFriends/FoodNFriends/ViewController/LocationDetailViewController.swift
+++ FoodNFriends/FoodNFriends/ViewController/LocationDetailViewController.swift
@@ -7,30 +7,47 @@
 
 import Foundation
 import UIKit
+import GoogleMaps
+import MapKit
 
 class LocationDetailViewController : UIViewController {
-    
+    @IBOutlet weak var locationDesc: UILabel!
+    @IBOutlet weak var locationAddr: UILabel!
+    @IBOutlet weak var locationMapView: GMSMapView!
+
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     var location:Location?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set the selected location to local scope location variable
         location = appDelegate.location
         
-        nameTxt.text = location?.Name
-        descTxt.text = location?.Description
-        latTxt.text = location?.Latitiude
-        longTxt.text = location?.Longitude
-    }
-    
-    
-    @IBOutlet weak var nameTxt: UILabel!
-    @IBOutlet weak var descTxt: UILabel!
-    @IBOutlet weak var latTxt: UILabel!
-    @IBOutlet weak var longTxt: UILabel!
-    
-    @IBAction func backBtn(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+        // Create a GMSCameraPosition that tells the map to display the selected location based on its longitude and latitude
+        locationMapView.camera = GMSCameraPosition.camera(withLatitude: Double(location!.Latitiude)!, longitude: Double(location!.Longitude)!, zoom: 15, bearing: 0, viewingAngle: 0)
+        
+        if appDelegate.mapType == "normal" {
+            locationMapView.mapType = .normal
+        } else if appDelegate.mapType == "hybrid" {
+            locationMapView.mapType = .hybrid
+        } else {
+            locationMapView.mapType = .terrain
+        }
+        
+        locationMapView.animate(toViewingAngle: 20)
+        
+        // Create a marker in the center of the map to show the current selected location
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: Double(location!.Latitiude)!, longitude: Double(location!.Longitude)!)
+        marker.title = location?.Description
+        marker.snippet = location?.Name
+        marker.icon = GMSMarker.markerImage(with: .purple)
+        marker.map = locationMapView
+        
+        // Load the text data into the view labels
+        locationDesc.text = location?.Description
+        locationAddr.text = location?.Name
     }
 }
 
